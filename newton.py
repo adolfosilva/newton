fee1da5bead9a8b3d6c83914dd07ff942fa378d8
Adolfo Silva <code@anomi.co>
2016-05-16 21:29:58 +0100

    Add unit test for iters method

--- newton.py
+++ newton.py
@@ -59,7 +59,8 @@ class Newton:
         Iteração atual: {}
         Iteração máxima: {}
         Tipo de Erro: {}
-        '''.format(self.f, self.df, self.ddf, self.intervalo, self.x0, self.erro_max, self.iter_atual, self.iter_max, self.error_str())
+        '''.format(self.f, self.df, self.ddf, self.intervalo, self.x0, self.erro_max,
+                self.iter_atual, self.iter_max, self.error_str())
 
     def error_str(self):
         return 'Absoluto' if self.err_type == AbsoluteError else 'Relativo'
@@ -121,7 +122,7 @@ class Newton:
     # TODO: test
     def iters(self, n):
         """Retorna as primeiras n linhas de resultados."""
-        return [row for row in self]
+        return [self.next() for _ in range(n)]
 
 class TestErrors(unittest.TestCase):
     f1 = sympy.sympify("x**4 + x - 1")
@@ -143,6 +144,16 @@ class TestErrors(unittest.TestCase):
         self.assertEqual((1,0.8,0.2096,3.048,0.7312,0.0688), n.next())
         self.assertEqual((2,0.7312,0.0171,2.5638,0.7245,0.0067), n.next())
         with self.assertRaises(MaxIterReached): n.next()
+
+    def test_iters(self):
+        from copy import copy
+        n = Newton(f=self.f1, intervalo=(0.0,1.0), erro_max=0.0005, iter_max=3, err_type=RelativeError, casas_decimais=4)
+        n0, n1, n2, n3 = copy(n), copy(n), copy(n), copy(n)
+        self.assertEqual([], n1.iters(0))
+        self.assertEqual([(0,1.0,1.0,5.0,0.8,None)], n1.iters(1))
+        self.assertEqual([(0,1.0,1.0,5.0,0.8,None),(1,0.8,0.2096,3.048,0.7312,0.0688)], n2.iters(2))
+        self.assertEqual([(0,1.0,1.0,5.0,0.8,None),(1,0.8,0.2096,3.048,0.7312,0.0688),(2, 0.7312, 0.0171, 2.5638, 0.7245, 0.0067)], n3.iters(3))
+
 
 if __name__ == '__main__':
     # unittest.main()
